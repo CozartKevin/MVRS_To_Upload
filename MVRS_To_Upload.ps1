2c82ff2b0ca41bac687aa540b09cd79c712a3f3d
Kevin Cozart <Cozartkevin@gmail.com>
2024-05-13 14:23:15 -0700

    Update MVRS_To_Upload.ps1

--- MVRS_To_Upload.ps1
+++ MVRS_To_Upload.ps1
@@ -24,7 +24,7 @@
 # Copyright:
 #   MIT License
 #
-#   Copyright (c) [2024] Kevin Cozart
+#   Copyright (c) [2024] Kevin Cozart.
 #
 #   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
 #   associated documentation files (the "Software"), to deal in the Software without restriction, including 
